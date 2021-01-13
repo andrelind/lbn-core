ab6a63ebde2bd7a98c80cea4f0e424cad5dcd852
André Lind <andre@lindapp.se>
2021-01-13 10:02:04 +0100

    Remove sagas

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import * as Actions from "./actions";
-import * as Assets from "./assets";
-import * as Colors from "./assets/colors";
-import * as Helpers from "./helpers";
-import * as Loader from "./loader";
-import * as Reducers from "./reducers";
-import * as Requests from "./requests";
-import * as Sagas from "./sagas";
-import * as State from "./state";
+import * as Actions from './actions';
+import * as Assets from './assets';
+import * as Colors from './assets/colors';
+import * as Helpers from './helpers';
+import * as Loader from './loader';
+import * as Reducers from './reducers';
+import * as Requests from './requests';
+import * as State from './state';
 
 export const actions = Actions;
 export const assets = Assets;
@@ -15,7 +14,6 @@ export const helpers = Helpers;
 export const loader = Loader;
 export const reducers = Reducers;
 export const requests = Requests;
-export const sagas = Sagas;
 export const state = State;
 
 export default {
@@ -26,6 +24,5 @@ export default {
   loader: Loader,
   reducers: Reducers,
   requests: Requests,
-  sagas: Sagas,
   state: State,
 };
